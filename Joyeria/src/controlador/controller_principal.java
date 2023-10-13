c475059019fda6f84fc6dcc60f1ca927be22b296
leocarlos10 <leocarlosospinacausil10@gmail.com>
2023-10-13 16:41:59 -0500

    renaciendo

---
+++ Joyeria/src/controlador/controller_principal.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package controlador;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+
+/**
+ * 
+ *
+ * @author USUARIO
+ */
+public class controller_principal implements Initializable {
+
+     @FXML
+    private ComboBox<String> combo_categorias;
+
+    @FXML
+    private ImageView usuario;
+
+    @FXML
+    private ComboBox<?> combo_inv_opciones;
+
+    @FXML
+    void event_op_usuario(MouseEvent event) {
+
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+        combo_categorias.getItems().addAll("Anillos para hombre","anillos para mujer","collares para hombre","collares para mujer");
+        
+    }    
+    
+}
