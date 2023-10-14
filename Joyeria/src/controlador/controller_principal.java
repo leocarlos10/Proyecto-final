e350ee42f4c066d7689e4d6801eb58f64177eedd
leocarlos10 <leocarlosospinacausil10@gmail.com>
2023-10-14 14:29:51 -0500

    terminando las funcionalidades del principal

--- Joyeria/src/controlador/controller_principal.java
+++ Joyeria/src/controlador/controller_principal.java
@@ -10,6 +10,7 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Cursor;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
@@ -27,13 +28,13 @@ public class controller_principal implements Initializable {
     
     // componentes FXML
      @FXML
-    private ComboBox<String> combo_categorias;
+    private ComboBox<Button> combo_categorias;
 
     @FXML
     private ImageView usuario;
     
      @FXML
-    private ComboBox<String> combo_opciones;
+    private ComboBox<Button> combo_opciones;
      
      @FXML
     private TextField text_buscar;
@@ -44,14 +45,14 @@ public class controller_principal implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         
         // cargamos el comboBox de las categorias y el de las opciones de la cuenta
-        combo_categorias.getItems().addAll("Anillos para hombre","anillos para mujer","collares para hombre","collares para mujer");
-        combo_opciones.getItems().addAll("Iniciar sesion","Registrarse","Historial de compra");
+        Cargar_combo_categorias();
+        Cargar_combo_opciones();
         // con este hacemos que cuando el cursor este sobre los comboBox se vuelvan una manito.
         combo_opciones.setCursor(Cursor.HAND);
         combo_categorias.setCursor(Cursor.HAND);
     }    
     
-    // eventos
+    // eventos 
     
     // con este evento manejo la visibilidad del TexField buscar
     @FXML
@@ -76,5 +77,30 @@ public class controller_principal implements Initializable {
             text_buscar.setVisible(false);
         }
         // agregar el codigo para que muestre la venta del producto buscado
+    }
+    
+    // metodos que cargan los comboBox.
+    public void Cargar_combo_categorias(){
+        
+        Button anillos_hombre = new Button("Anillos de hombre");
+        anillos_hombre.setPrefWidth(150);
+        Button anillos_mujer = new Button("Anillos de mujer");
+         anillos_mujer.setPrefWidth(150);
+        Button cadenas_hombre = new Button("Cadenas de hombre");
+         cadenas_hombre.setPrefWidth(150);
+        Button cadenas_mujer = new Button("Cadenas de mujer");
+         cadenas_mujer.setPrefWidth(150);
+        combo_categorias.getItems().addAll(anillos_hombre,anillos_mujer,cadenas_hombre,cadenas_mujer);
+    }
+    
+     public void Cargar_combo_opciones(){
+        
+        Button incio_sesion = new Button("Inicio de sesion");
+        incio_sesion.setPrefWidth(150);
+        Button registro = new Button("Registrarse");
+        registro.setPrefWidth(150);
+        Button historial = new Button("Historial de compra");
+        historial.setPrefWidth(150);
+        combo_opciones.getItems().addAll(incio_sesion,registro,historial);
     }
 }
