1d9782cb42ed655a0ee29107d0055db278998763
leocarlos10 <leocarlosospinacausil10@gmail.com>
2023-11-21 15:48:51 -0500

    actualizacion reciente

--- Joyeria/src/controlador/Catalogo_anillosmujerController.java
+++ Joyeria/src/controlador/Catalogo_anillosmujerController.java
@@ -7,8 +7,14 @@ package controlador;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javax.swing.JOptionPane;
 
 /**
  * FXML Controller class
@@ -22,16 +28,33 @@ public class Catalogo_anillosmujerController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+    
+     @FXML
+    private void event_info_vista1(MouseEvent event) {
+         try {
+             
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/vista_info_anillosmujer/info_anillomujer1.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.setScene(scene);
+            stage.show();
+            Info_anillomujer1Controller controlador = loader.getController();
+            controlador.setStage(stage);
+           
+            
+         } catch (Exception e) {
+             JOptionPane.showMessageDialog(null, " Error "+e);
+         }
+    }
 
     @FXML
     private void event_info_vista2(MouseEvent event) {
+        
     }
-
-    @FXML
-    private void event_info_vista1(MouseEvent event) {
-    }
-
+    
     @FXML
     private void event_info_vista3(MouseEvent event) {
     }
