794dfcc374c4d2b192f95abbfa2f518b1b5b9cdb
leocarlos10 <leocarlosospinacausil10@gmail.com>
2023-12-05 22:52:35 -0500

    renaciendo

--- Joyeria/src/controlador/Info_anillomujer1Controller.java
+++ Joyeria/src/controlador/Info_anillomujer1Controller.java
@@ -4,17 +4,11 @@
  */
 package controlador;
 
-import Logica_Pila.Pila_Producto;
-import Logica_Pila.Producto;
-import Logica_listasencilla.Lista_producto;
-import Logica_listasencilla.nodo_producto;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -26,9 +20,6 @@ import javafx.stage.Stage;
 public class Info_anillomujer1Controller implements Initializable {
 
     Stage stage;
-    Pila_Producto p = new Pila_Producto();
-    Lista_producto listaP = new Lista_producto();
-   
 
     public void setStage(Stage stage) {
 
@@ -39,10 +30,6 @@ public class Info_anillomujer1Controller implements Initializable {
     private ComboBox<String> combo_talla;
     @FXML
     private ComboBox<String> combo_cantidad;
-    @FXML
-    private Label nombre_producto;
-    @FXML
-    private Label precio_producto;
 
     /**
      * Initializes the controller class.
@@ -56,43 +43,6 @@ public class Info_anillomujer1Controller implements Initializable {
     @FXML
     private void event_volver(MouseEvent event) {
         stage.close();
-    }
-
-    @FXML
-    private void event_agregar_carrito(ActionEvent event) {
-        
-          // primero creamos el objeto
-        Producto pro = new Producto(
-                nombre_producto.getText(),
-                precio_producto.getText(),
-                combo_talla.getValue(),
-                combo_cantidad.getValue());
-
-            // lo guardamos en le fichero.
-            try {
-                p.guardar_P_fichero(pro);
-            } catch (Exception e) {
-                System.out.println("no se pudo guardar el producto en el carrito.");
-            }
-        
-        p.aviso_info("INFO", "Producto agregado al carrito");
-        
-    }
-
-    @FXML
-    private void event_agregar_favoritos(ActionEvent event) {
-        
-         // primero creamos el objeto
-         nodo_producto pro = new nodo_producto(nombre_producto.getText(),precio_producto.getText());
-
-            // lo guardamos en le fichero.
-            try {
-               listaP.guardar_P_favoritos(pro);
-            } catch (Exception e) {
-                System.out.println("no se pudo guardar el producto en favoritos.");
-            }
-        
-        p.aviso_info("INFO", "Producto agregado a favoritos");
     }
 
 }
